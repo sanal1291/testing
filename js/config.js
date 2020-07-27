f8f66c3772da3837895a6094856d44c8c3bfcf22
Sanal <sanalmm>
2020-07-27 19:56:40 +0530

    aws clone

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_4nYjPWZg7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3nb5v3i1j51avevi3jkbbb98cp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://master.d27ds5pu8i3cvn.amplifyapp.com/test' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
