097b8f851f50fbc321daa997a552d714c7c2d010
Sanal <sanalmm>
2020-07-28 11:29:38 +0530

    adsd

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_4nYjPWZg7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3nb5v3i1j51avevi3jkbbb98cp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'https://master.d27ds5pu8i3cvn.amplifyapp.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
     }
 };
